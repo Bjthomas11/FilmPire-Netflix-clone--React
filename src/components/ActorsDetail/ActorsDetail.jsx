7cf5274f5e535e4bb18e2c9df7a63f1777cf39a7
Brian Thomas <brianthomas571@gmail.com>
2022-05-31 10:39:57 -0500

    base mui styling setup hook

---
+++ src/components/ActorsDetail/ActorsDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ActorsDetail = () => {
+  return <div>ActorsDetail</div>;
+};
+
+export default ActorsDetail;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ActorsDetail } from "./ActorsDetail/ActorsDetail";
+export { default as MovieDetail } from "./MovieDetail/MovieDetail";
+export { default as Movies } from "./Movies/Movies";
+export { default as Nav } from "./Nav/Nav";
+export { default as ProfileDetail } from "./ProfileDetail/ProfileDetail";
